3176b5ed717fabc524f9c46e23ffaca623f62289
James B <james.baster@opendataservices.coop>
2024-10-23 10:55:26 +0100

    move processing to seperate function for easy testing later

--- humanitarian_data_exchange/pull_and_check_ckan.py
+++ humanitarian_data_exchange/pull_and_check_ckan.py
@@ -17,60 +17,7 @@ def pull_and_check_ckan():
     problems = []
     # Loop
     for ckan_dataset in data.get("result").get("results"):
-        # Handy Print Debugging
-        # print("------------------------------------------------")
-        # print(json.dumps(ckan_dataset, indent=2))
-        # Check data
-        if ckan_dataset.get("caveats") != settings.CKAN_CAVEATS:
-            problems.append(
-                "Resource " + ckan_dataset.get("name") + " has the wrong caveat"
-            )
-        tag_ids = [x.get("id") for x in ckan_dataset["tags"]]
-        for tag_id in settings.CKAN_TAG_IDS:
-            if tag_id not in tag_ids:
-                problems.append(
-                    "Resource "
-                    + ckan_dataset.get("name")
-                    + " does not have the expected tag "
-                    + tag_id
-                )
-        # Make out data
-        out_data = {
-            "id": ckan_dataset.get("id"),
-            "title": ckan_dataset.get("title"),
-            "resources": [],
-            "tags": [],
-        }
-        if out_data["title"].startswith(settings.CKAN_TITLE_START):
-            out_data["title"] = out_data["title"][len(settings.CKAN_TITLE_START) :]
-        else:
-            problems.append(
-                "Resource "
-                + ckan_dataset.get("name")
-                + " starts with a different title"
-            )
-        for ckan_resource in ckan_dataset.get("resources"):
-            out_resource = {
-                "description": ckan_resource.get("description"),
-                "url": ckan_resource.get("url"),
-            }
-            out_data["resources"].append(out_resource)
-        for ckan_tag in ckan_dataset.get("tags"):
-            if ckan_tag.get("id") not in settings.CKAN_TAG_IDS:
-                out_tag = {"id": ckan_tag.get("id"), "name": ckan_tag.get("name")}
-                out_data["tags"].append(out_tag)
-        ckan_dataset_solr_additions = json.loads(
-            ckan_dataset.get("solr_additions", "{}")
-        )
-        # Check data (after processing has been done)
-        if ckan_dataset_solr_additions.get("countries") != [out_data["title"]]:
-            problems.append(
-                "Resource "
-                + ckan_dataset.get("name")
-                + " has a different solr_additions/countries! "
-                + str(ckan_dataset_solr_additions.get("countries"))
-            )
-        # Write out data
+        out_data, dataset_problems = _process_ckan_dataset(ckan_dataset)
         with open(
             os.path.join(
                 settings.DATASETS_DIRECTORY, ckan_dataset.get("name") + ".yaml"
@@ -78,8 +25,57 @@ def pull_and_check_ckan():
             "w",
         ) as fp:
             yaml.dump(out_data, fp, indent=4, sort_keys=True)
+        for dataset_problem in dataset_problems:
+            problems.append(
+                "Resource " + ckan_dataset.get("name") + ": " + dataset_problem
+            )
     # return
     return problems
+
+
+def _process_ckan_dataset(ckan_dataset):
+    problems = []
+    # Handy Print Debugging
+    # print("------------------------------------------------")
+    # print(json.dumps(ckan_dataset, indent=2))
+    # Check data
+    if ckan_dataset.get("caveats") != settings.CKAN_CAVEATS:
+        problems.append("wrong caveat")
+    tag_ids = [x.get("id") for x in ckan_dataset["tags"]]
+    for tag_id in settings.CKAN_TAG_IDS:
+        if tag_id not in tag_ids:
+            problems.append("does not have the expected tag " + tag_id)
+    # Make out data
+    out_data = {
+        "id": ckan_dataset.get("id"),
+        "title": ckan_dataset.get("title"),
+        "resources": [],
+        "tags": [],
+    }
+    if out_data["title"].startswith(settings.CKAN_TITLE_START):
+        out_data["title"] = out_data["title"][len(settings.CKAN_TITLE_START) :]
+    else:
+        problems.append("starts with a different title")
+    for ckan_resource in ckan_dataset.get("resources"):
+        out_resource = {
+            "description": ckan_resource.get("description"),
+            "url": ckan_resource.get("url"),
+        }
+        out_data["resources"].append(out_resource)
+    for ckan_tag in ckan_dataset.get("tags"):
+        if ckan_tag.get("id") not in settings.CKAN_TAG_IDS:
+            out_tag = {"id": ckan_tag.get("id"), "name": ckan_tag.get("name")}
+            out_data["tags"].append(out_tag)
+    ckan_dataset_solr_additions = json.loads(ckan_dataset.get("solr_additions", "{}"))
+    # Check data (after processing has been done)
+    if ckan_dataset_solr_additions.get("countries") != [out_data["title"]]:
+        problems.append(
+            "has a different solr_additions/countries! "
+            + str(ckan_dataset_solr_additions.get("countries"))
+        )
+    # Write out data
+
+    return out_data, problems
 
 
 if __name__ == "__main__":
