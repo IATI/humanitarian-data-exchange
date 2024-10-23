b931cbf96605eadabafb215f2bf40a2a8f7f77b3
James B <james.baster@opendataservices.coop>
2024-10-23 11:53:17 +0100

    check resource URLs
    Changes strucutre of output data, puts country vars under a country dict to group them

--- humanitarian_data_exchange/pull_and_check_ckan.py
+++ humanitarian_data_exchange/pull_and_check_ckan.py
@@ -4,6 +4,7 @@ import os.path
 import requests
 import settings
 import yaml
+import urllib.parse
 
 
 def pull_and_check_ckan():
@@ -48,12 +49,18 @@ def _process_ckan_dataset(ckan_dataset):
     # Make out data
     out_data = {
         "id": ckan_dataset.get("id"),
-        "title": ckan_dataset.get("title"),
+        "country": {
+            "iso_2char": None,
+            "iso_3char": None,
+            "title": ckan_dataset.get("title"),
+        },
         "resources": [],
         "tags": [],
     }
-    if out_data["title"].startswith(settings.CKAN_TITLE_START):
-        out_data["title"] = out_data["title"][len(settings.CKAN_TITLE_START) :]
+    if out_data["country"]["title"].startswith(settings.CKAN_TITLE_START):
+        out_data["country"]["title"] = out_data["country"]["title"][
+            len(settings.CKAN_TITLE_START) :
+        ]
     else:
         problems.append("starts with a different title")
     if len(ckan_dataset.get("resources")) != 1:
@@ -70,13 +77,44 @@ def _process_ckan_dataset(ckan_dataset):
             description = description[: -len(settings.CKAN_RESOURCE_V1_DESCRIPTION_END)]
         else:
             problems.append("resource ends with a different description")
-        if description != out_data["title"]:
+        if description != out_data["country"]["title"]:
             problems.append(
                 "resource description country does not match title: " + description
             )
-        out_resource = {
-            "url": ckan_resource.get("url"),
-        }
+        if ckan_resource.get("url") != ckan_resource.get("download_url"):
+            problems.append("resource has different url and download_url")
+        if ckan_resource.get("url") != ckan_resource.get("hdx_rel_url"):
+            problems.append("resource has different url and hdx_rel_url")
+        url_bits = urllib.parse.urlparse(ckan_resource.get("url"))
+        url_query_bits = urllib.parse.parse_qs(url_bits.query)
+        out_data["country"]["iso_2char"] = url_query_bits.get("add-value03", []).pop()
+        out_data["country"]["iso_3char"] = url_query_bits.get("add-value02", []).pop()
+        if url_bits.scheme != settings.CKAN_RESOURCE_V1_URL_SCHEME:
+            problems.append("resource url has wrong scheme " + url_bits.scheme)
+        if url_bits.netloc != settings.CKAN_RESOURCE_V1_URL_NETLOC:
+            problems.append("resource url has wrong netloc " + url_bits.netloc)
+        if url_bits.path != settings.CKAN_RESOURCE_V1_URL_PATH.format(
+            name=ckan_dataset["name"]
+        ):
+            problems.append("resource url has wrong path " + url_bits.path)
+        if url_bits.params != settings.CKAN_RESOURCE_V1_URL_PARAMS:
+            problems.append("resource url has wrong params " + url_bits.path)
+        if url_bits.fragment != settings.CKAN_RESOURCE_V1_URL_FRAGMENT:
+            problems.append("resource url has wrong fragment " + url_bits.fragment)
+        for key, value in settings.CKAN_RESOURCE_V1_URL_QUERY_PARSED.items():
+            if url_query_bits.get(key) != value:
+                problems.append(
+                    "resource url has wrong query bit "
+                    + key
+                    + " Value is "
+                    + str(value)
+                )
+        dportal_url = url_query_bits.get("url", []).pop()
+        if dportal_url != settings.CKAN_RESOURCE_V1_DPORTAL_URL.format(
+            country_iso_2char=out_data["country"]["iso_2char"]
+        ):
+            problems.append("Resource D-Portal URL is wrong!")
+        out_resource = {"id": ckan_resource["id"]}
         out_data["resources"].append(out_resource)
     for ckan_tag in ckan_dataset.get("tags"):
         if ckan_tag.get("id") not in settings.CKAN_TAG_IDS:
@@ -84,7 +122,7 @@ def _process_ckan_dataset(ckan_dataset):
             out_data["tags"].append(out_tag)
     ckan_dataset_solr_additions = json.loads(ckan_dataset.get("solr_additions", "{}"))
     # Check data (after processing has been done)
-    if ckan_dataset_solr_additions.get("countries") != [out_data["title"]]:
+    if ckan_dataset_solr_additions.get("countries") != [out_data["country"]["title"]]:
         problems.append(
             "has a different solr_additions/countries! "
             + str(ckan_dataset_solr_additions.get("countries"))
--- humanitarian_data_exchange/settings.py
+++ humanitarian_data_exchange/settings.py
@@ -14,6 +14,92 @@ CKAN_TITLE_START = "Current IATI aid activities in "
 
 CKAN_RESOURCE_V1_DESCRIPTION_START = "Currently-active IATI activities in "
 CKAN_RESOURCE_V1_DESCRIPTION_END = ", in 3W/4W style with HXL hashtags. This dataset has one row for each unique activity/sector/location, so the same IATI activity may occupy multiple rows: use the #activity+id column to count unique activities."
+CKAN_RESOURCE_V1_URL_SCHEME = "https"
+CKAN_RESOURCE_V1_URL_NETLOC = "proxy.hxlstandard.org"
+CKAN_RESOURCE_V1_URL_PATH = "/data/download/{name}.csv"
+CKAN_RESOURCE_V1_URL_PARAMS = ""
+CKAN_RESOURCE_V1_URL_FRAGMENT = ""
+CKAN_RESOURCE_V1_URL_QUERY_PARSED = {
+    "filter01": ["cut"],
+    "cut-skip-untagged01": ["on"],
+    "filter02": ["add"],
+    "add-tag02": ["#country+code+v_iso3"],
+    # "add-value02": ["UMI"],      This changes per data set
+    "add-header02": ["iso3"],
+    "filter03": ["add"],
+    "add-tag03": ["#country+code+v_iso2"],
+    # "add-value03": ["UM"],      This changes per data set
+    "add-header03": ["iso2"],
+    "filter04": ["add"],
+    "add-tag04": ["#activity+url"],
+    "add-value04": ["{{#activity+id}}"],
+    "add-header04": ["activity_id"],
+    "filter05": ["replace"],
+    "replace-pattern05": ["^.*aid=(.+)$"],
+    "replace-regex05": ["on"],
+    "replace-value05": ["\\1"],
+    "replace-tags05": ["#activity+id"],
+    "tagger-match-all": ["on"],
+    "tagger-01-header": ["aid"],
+    "tagger-01-tag": ["#activity+id"],
+    "tagger-02-header": ["reporting"],
+    "tagger-02-tag": ["#org+reporting+name"],
+    "tagger-03-header": ["reporting_ref"],
+    "tagger-03-tag": ["#org+reporting+id"],
+    "tagger-04-header": ["funder_ref"],
+    "tagger-04-tag": ["#org+funder+id"],
+    "tagger-05-header": ["title"],
+    "tagger-05-tag": ["#activity+title"],
+    "tagger-07-header": ["status_code"],
+    "tagger-07-tag": ["#status"],
+    "tagger-08-header": ["day_start"],
+    "tagger-08-tag": ["#date+start"],
+    "tagger-09-header": ["day_end"],
+    "tagger-09-tag": ["#date+end"],
+    "tagger-11-header": ["description"],
+    "tagger-11-tag": ["#description"],
+    "tagger-12-header": ["commitment"],
+    "tagger-12-tag": ["#value+committed+usd"],
+    "tagger-13-header": ["spend"],
+    "tagger-13-tag": ["#value+spent+usd"],
+    "tagger-14-header": ["commitment_eur"],
+    "tagger-14-tag": ["#value+committed+eur"],
+    "tagger-15-header": ["spend_eur"],
+    "tagger-15-tag": ["#value+spent+eur"],
+    "tagger-16-header": ["commitment_gbp"],
+    "tagger-16-tag": ["#value+committed+gbp"],
+    "tagger-17-header": ["spend_gbp"],
+    "tagger-17-tag": ["#value+spent+gbp"],
+    "tagger-18-header": ["commitment_cad"],
+    "tagger-18-tag": ["#value+committed+cad"],
+    "tagger-19-header": ["spend_cad"],
+    "tagger-19-tag": ["#value+spent+cad"],
+    "tagger-21-header": ["country_code"],
+    "tagger-21-tag": ["#country+name"],
+    "tagger-22-header": ["country_percent"],
+    "tagger-22-tag": ["#indicator+country_allocation+pct"],
+    "tagger-23-header": ["sector_group"],
+    "tagger-23-tag": ["#sector"],
+    "tagger-24-header": ["sector_code"],
+    "tagger-24-tag": ["#subsector"],
+    "tagger-25-header": ["sector_percent"],
+    "tagger-25-tag": ["#indicator+subsector_allocation+pct"],
+    "tagger-26-header": ["location_code"],
+    "tagger-26-tag": ["#loc+code"],
+    "tagger-29-header": ["location_name"],
+    "tagger-29-tag": ["#loc+name"],
+    "tagger-30-header": ["location_longitude"],
+    "tagger-30-tag": ["#geo+lon"],
+    "tagger-31-header": ["location_latitude"],
+    "tagger-31-tag": ["#geo+lat"],
+    "tagger-32-header": ["location_precision"],
+    "tagger-32-tag": ["#geo+precision+code"],
+    "tagger-33-header": ["location_percent"],
+    "tagger-33-tag": ["#indicator+location_allocation+pct"],
+    "header-row": ["1"],
+    # "url": "...",      This changes per data set
+}
+CKAN_RESOURCE_V1_DPORTAL_URL = "http://d-portal.org/dquery?form=csv&human=1&sql=select * from act left join country using (aid) left join sector using (aid) left join location using (aid) where status_code%3D2 and country.country_code%3D'{country_iso_2char}' and day_end >%3D floor(extract(epoch from now())%2F(60*60*24)) order by day_start%2C day_end%2C reporting&_gl=1*avmz6m*_ga*MTQ0Nzk3MzM5OS4xNjY3MjI2ODk2*_ga_E60ZNX2F68*MTY4MTc0NjM3OC43OS4xLjE2ODE3NDYzODkuNDkuMC4w"
 
 DATASETS_DIRECTORY = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "..", "datasets"
